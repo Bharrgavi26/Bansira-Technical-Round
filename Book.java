1380263c81041b9bc558cfce9b1d35cb8d4b62dc
Bharrgavi26 <bharrgavi.t@gmail.com>
2024-07-14 17:04:20 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,75 @@
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private String genre;
+    private int publicationYear;
+    private boolean availability;
+    private String department;
+
+    public Book(String title, String author, String ISBN, String genre, int publicationYear, boolean availability, String department) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.genre = genre;
+        this.publicationYear = publicationYear;
+        this.availability = availability;
+        this.department = department;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
